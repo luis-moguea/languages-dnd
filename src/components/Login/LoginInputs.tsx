1f52e07cb961a23ab61cf30639786995a8860847
luis-moguea <luis.mogueadp@gmail.com>
2023-08-07 17:56:22 -0500

    style updates

--- src/components/Login/LoginInputs.tsx
+++ src/components/Login/LoginInputs.tsx
@@ -36,44 +36,76 @@ const LoginInputs = ({ user }: Props) => {
 
   return (
     <>
-      <NavBar />
-      <form onSubmit={preventReload}>
-        <InputGroup
-          border="solid"
-          borderWidth="1px"
-          borderColor="gray.300"
-          rounded="2xl"
-          padding="2em"
-          flexDirection="column"
-          mt="150px"
-          maxWidth="350px"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Input
-            type="text"
-            onChange={(event) => setName(event.target.value)}
-          ></Input>
-          <Text fontSize="11px" fontStyle="oblique" mb="10px">
-            User
-          </Text>
-          <Input
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          ></Input>
-          <Text fontSize="11px" fontStyle="oblique">
-            Password
-          </Text>
-          <Button rounded="3xl" mt="15px" onClick={handleSubmit}>
-            Sign In
-          </Button>
-        </InputGroup>
-      </form>
-      <Box>
-        {nameError && <Text>{nameError}</Text>}
-        {passwordError && <Text>{passwordError}</Text>}
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <NavBar />
+        <form onSubmit={preventReload}>
+          <InputGroup
+            border="solid"
+            borderWidth="1px"
+            borderColor="gray.300"
+            rounded="2xl"
+            padding="2em"
+            flexDirection="column"
+            mt="50px"
+            maxWidth="350px"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Box mb="10px">
+              <Text opacity="0.6" fontSize="9px" fontStyle="oblique">
+                User
+              </Text>
+              <Input
+                type="text"
+                onChange={(event) => setName(event.target.value)}
+              ></Input>
+            </Box>
+            <Box>
+              <Text opacity="0.6" fontSize="9px" fontStyle="oblique">
+                Password
+              </Text>
+              <Input
+                type="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              ></Input>
+            </Box>
+
+            <Button rounded="3xl" mt="15px" onClick={handleSubmit}>
+              Sign In
+            </Button>
+          </InputGroup>
+        </form>
+        <Box>
+          {nameError && (
+            <Text
+              maxW="250px"
+              color="red.300"
+              textAlign="center"
+              fontSize="10px"
+              mt="20px"
+            >
+              {nameError}
+            </Text>
+          )}
+          {passwordError && (
+            <Text
+              maxW="250px"
+              color="red.300"
+              textAlign="center"
+              fontSize="10px"
+              mt="20px"
+            >
+              {passwordError}
+            </Text>
+          )}
+        </Box>
       </Box>
     </>
   );
